2f9d05f0a48cd8b524fc753a2329682bf528589a
ErickSantos2000 <devErickFelipe@gmail.com>
2025-05-03 16:43:56 -0300

    teste

--- Main.java
+++ Main.java
@@ -1,15 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         Carro carro = new Carro("Nissan", "Skyline GTR R34", 2002, "Azul");
         BodyShop oficina = new BodyShop(carro);
-        carro.setOficina(oficina);
         ColorShop pintura = new ColorShop(carro);
         Menu menu = new Menu(carro, oficina, pintura);
 
+        carro.setOficina(oficina);
         menu.menuPrincipal();
 
     }
